6257c23584cc68c633464d5c8162989179cd6cdf
Julien Pivotto <roidelapluie@inuits.eu>
2020-07-16 19:10:16 +0200

    typo
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- lib/filters/prerelease_content.rb
+++ lib/filters/prerelease_content.rb
@@ -9,7 +9,7 @@ class PrerelaseContent < ::Nanoc::Filter
     doc = Nokogiri::HTML(content)
     # TODO(ts): We need to link to the same page or the first child without hardcoding /getting_started/.
     warning = %(<p>CAUTION: This page documents a pre-release version of #{params[:repository].split("/")[-1].split(".")[0].capitalize()}.
-      Check out the <a href="#{params[:prelease]}">latest stable version</a>.</p>)
+      Check out the <a href="#{params[:prerelease]}">latest stable version</a>.</p>)
 
     body = doc.css('body')
     if first = body.children.first

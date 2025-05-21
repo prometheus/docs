71415857b461d2c71b9f81de9704f5e3e0f6bfc1
Julius Volz <julius.volz@gmail.com>
2025-05-22 00:08:51 +0200

    Add Governance page
    Signed-off-by: Julius Volz <julius.volz@gmail.com>

---
+++ src/app/governance/page.tsx
@@ -0,0 +1,27 @@
+import PromMarkdown from "@/components/PromMarkdown";
+import TOC from "@/components/TOC";
+import { Box, Group, Title } from "@mantine/core";
+import { readFileSync } from "fs";
+
+export default function CommunityPage() {
+  const content = readFileSync(
+    `${process.cwd()}/src/app/governance/governance.md`,
+    "utf-8"
+  );
+
+  return (
+    <Group wrap="nowrap" align="flex-start" pos="relative" gap="xl">
+      <Box
+        pos="sticky"
+        top={0}
+        w="fit-content"
+        className="markdown-content"
+        data-pagefind-body
+      >
+        <Title order={1}>Governance</Title>
+        <PromMarkdown>{content}</PromMarkdown>
+      </Box>
+      <TOC />
+    </Group>
+  );
+}
